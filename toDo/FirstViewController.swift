9ab183fb0b7f009a1e560dd397d38ee91cb0bf9d
jurajmajerik <juro.majerik@gmail.com>
2017-02-27 17:30:38 +0100

    Initial Commit

---
+++ toDo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  toDo
+//
+//  Created by Juraj Majerik on 27/02/2017.
+//  Copyright © 2017 Juraj Majerik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
