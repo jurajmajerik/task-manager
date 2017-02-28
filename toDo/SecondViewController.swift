bd009370b05c1cb9ada9aa55bb4cd47dcc29a5a2
jurajmajerik <juro.majerik@gmail.com>
2017-02-28 20:35:33 +0100

    first commit

--- toDo/SecondViewController.swift
+++ toDo/SecondViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func addItem(_ sender: AnyObject)
+    {
+        if (input.text != "")
+        {
+            list.append(input.text!)
+            input.text = ""
+            
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
